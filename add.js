f2f836a0b0d36812aaa6ee47dadd022770c4497e
Vaibhav Agrahari <vaibhavagrahari599@gmail.com>
2025-04-01 10:12:59 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,13 @@
+//const add=(a,b)=>{
+   export const add=(a,b)=>{
+        return a+b; //not hoisted
+    }
+   export const sub=(a,b)=>{
+        return a-b;
+    }
+   // module.exports = {add,sub};
+    //module.exports=addwitharrow;
+    //function add(a,b){
+   // return a+b;//hoisted
+//}
+//console.log(add(3,4));
---
+++ index.js
@@ -0,0 +1,5 @@
+//const calc=require("./add.js");
+import {add,sub} from "./add.js";
+console.log(add);
+console.log(add(3,7));
+console.log(sub(6,7));
